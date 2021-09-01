d473409980997528502af39f1f430e886b02fc8a
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-01 16:42:14 -0300

    C3 - Adiconado requisito 3 e 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <MovieList />
+    </>
   );
 }
 
