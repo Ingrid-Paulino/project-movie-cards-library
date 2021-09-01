58208844db303b35a61b18f9f807e9258740f937
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-01 15:25:25 -0300

    C1 - Adicionado Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h3>Hello!!!</h3>
+    );
+  }
+}
+
+export default Header;
