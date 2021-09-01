48e9c7b3cf3ccc4ee908265e70b634b1d5b96103
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-01 17:02:06 -0300

    C5 - Adicionado requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 
 export default class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+
     return (
-      <p>test</p>
+      <section>
+        <h2>{movie.title}</h2>
+        <h3>{movie.subtitle}</h3>
+        <p>{movie.storyline}</p>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <p>{movie.rating}</p>
+      </section>
     );
   }
 }
